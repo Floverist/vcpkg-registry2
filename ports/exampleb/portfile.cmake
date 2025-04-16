417c4e8f00f5a74f56252f56851b96468454b98f
Prredator <vlastovv@gmail.com>
2025-04-17 00:04:22 +0300

    _

--- ports/exampleb/portfile.cmake
+++ ports/exampleb/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 #vcpkg_cmake_config_fixup(PACKAGE_NAME "hello-world" CONFIG_PATH "share/exampleb")
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "exampleb")
 #vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
